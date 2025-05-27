a19daa161e068d98386a2f1c342fa6818ac4517e
co-004 <cocolin40401234@gmail.com>
2025-05-27 22:24:54 +0800

    0527co-10

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/jaso_quiz/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 });
